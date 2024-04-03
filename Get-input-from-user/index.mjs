6a51ade1ea7b470910810b2c44811cfdf4357471
Mohammad Taghinejad <smt.pv.page@gmail.com>
2024-04-03 18:38:27 +0330

    Feat: read query parameters with nodeJS.

--- Get-input-from-user/index.mjs
+++ Get-input-from-user/index.mjs
@@ -1,7 +1,14 @@
 import http from "http";
-
+import url from "url";
 const server = http.createServer((req, res) => {
-    res.write("Hello from Node.js HTTP server!");
+    const urlData = url.parse(req.url, true);
+    const { name } = urlData.query;
+
+    const message = name
+        ? `Hello ${name}!`
+        : "Hello from Node.js HTTP server!"
+
+    res.write(message);
     res.end();
 });
 
