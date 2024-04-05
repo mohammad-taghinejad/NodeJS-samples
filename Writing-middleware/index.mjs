e94c0fe756626860a00c033a8f7c9b774a4ef01e
Mohammad Taghinejad <smt.pv.page@gmail.com>
2024-04-05 17:14:12 +0330

    Feat: The say-hello route was added.

--- Writing-middleware/index.mjs
+++ Writing-middleware/index.mjs
@@ -16,6 +16,10 @@ app.get('/bye', (req, res) => {
     res.send("Good bye!");
 });
 
+app.get('/say-hello/:name', (req, res) => {
+    res.send(`Hello ${req.params.name}`);
+});
+
 app.listen(port, () => {
     console.log(`Our server is listening on port ${port}`);
 });
